1db977cfb4249602e6d2f9cee0dd052632ec80e2
lafar6502 <rafal@cogit.pl>
2015-03-26 19:27:44 +0100

    sv

--- test_srv/routes/companies.js
+++ test_srv/routes/companies.js
@@ -2,15 +2,16 @@
 var router = express.Router();
 
 var data = [
-{lat:43.07493, lng:-89.381388, name: "Firma X", address1: "", address2: ""},
-{lat:43.04993, lng:-89.441388, name: "Inna firma", address1: "", address2: ""},
-{lat:43.03493, lng:-89.531388, name: "FirmaZ", address1: "", address2: ""},
-{lat:42.97393, lng:-89.841388, name: "ATO Firma sp zoo", address1: "", address2: ""},
-{lat:42.94238, lng:-88.883211, name: "X i Y oraz synowie, oops córki", address1: "", address2: ""},
-{lat:42.95321, lng:-88.992839, name: "Chromosom ltd", address1: "", address2: ""}
+{lat:43.07493, lng:-89.381388, name: "Firma X", city:"Warszawa", address1: "", address2: ""},
+{lat:43.04993, lng:-89.441388, name: "Inna firma", city:"Warszawa", address1: "", address2: ""},
+{lat:43.03493, lng:-89.531388, name: "FirmaZ", city:"Poznań", address1: "", address2: ""},
+{lat:42.97393, lng:-89.841388, name: "ATO Firma sp zoo", city:"Wrocław", address1: "", address2: ""},
+{lat:42.94238, lng:-88.883211, name: "X i Y oraz synowie, oops córki", city:"Poznań", address1: "", address2: ""},
+{lat:42.95321, lng:-88.992839, name: "Chromosom ltd", city:"Rzeszów", address1: "", address2: ""}
 ]
 /* GET home page. */
 router.get('/', function(req, res) {
+	res.set('Access-Control-Allow-Origin', 'http://localhost:8100');
   res.json(data);
 });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,62 +1,63 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function ($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+.controller('ChatsCtrl', function ($scope, Chats) {
+	$scope.chats = Chats.all();
+	$scope.remove = function (chat) {
+		Chats.remove(chat);
+	}
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+	$scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function ($scope) {
+	$scope.settings = {
+		enableFriends : true
+	};
 })
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
+.controller('MapCtrl', function ($scope, $ionicLoading, $http) {
 	console.log('map ctrl loading');
-  $scope.mapCreated = function(map) {
-	  console.log('map created');
-    $scope.map = map;
-    
-     var site = new google.maps.LatLng(55.9879314,-4.3042387);
-     var hospital = new google.maps.LatLng(55.8934378,-4.2201905);
-      var marker = new google.maps.Marker({
-          position: site,
-          map: map,
-          title: 'Strathblane (Job Location)'
-        });
-        
-        var hospitalRoute = new google.maps.Marker({
-          position: hospital,
-          map: map,
-          title: 'Hospital (Stobhill)'
-        });
-  };
+	$scope.mapCreated = function (map) {
+		console.log('map created');
+		$scope.map = map;
 
-  $scope.centerOnMe = function () {
-    console.log("Centering");
-    if (!$scope.map) {
-      return;
-    }
+		$http.get('http://localhost:3000/companies')
+			.success(function(data, status, headers, config) {
+				console.log('got data', data);
+				for (var i=0; i<data.length; i++) {
+					var site = new google.maps.LatLng(data[i].lat, data[i].lng);
+					var marker = new google.maps.Marker({position: site, map: map,  title: data[i].name});
+				};
+				$scope.map.setCenter(new google.maps.LatLng(data[0].lat, data[0].lng));
+			})
+			.error(function(data, status, headers, config) {
+				console.log('got error', arguments);
+			});
+		
+		//$scope.centerOnMe();
+	};
 
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
+	$scope.centerOnMe = function () {
+		console.log("Centering");
+		if (!$scope.map) {
+			return;
+		}
 
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      console.log('Got pos', pos);
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function (error) {
-      alert('Unable to get location: ' + error.message);
-    });
-  };
+		$scope.loading = $ionicLoading.show({
+				content : 'Getting current location...',
+				showBackdrop : false
+			});
+
+		navigator.geolocation.getCurrentPosition(function (pos) {
+			console.log('Got pos', pos);
+			$scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+			$scope.loading.hide();
+		}, function (error) {
+			alert('Unable to get location: ' + error.message);
+		});
+	};
 });
