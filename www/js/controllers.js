e74fe3fe886bcb58a6e3c7918315d749536bb4f5
lafar6502 <rafal@cogit.pl>
2015-03-26 20:18:54 +0100

    sv

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,6 +21,26 @@ angular.module('starter.controllers', [])
 
 .controller('MapCtrl', function ($scope, $ionicLoading, $http) {
 	console.log('map ctrl loading');
+	
+	var configureMarker = function(mark, map) {
+		var site = new google.maps.LatLng(mark.lat, mark.lng);
+		var marker = new google.maps.Marker({position: site, map: map,  title: mark.name});
+		google.maps.event.addListener(marker, 'click', function() {
+			var cnt = [
+				"<b>" + mark.name + "</b> <br/>",
+				"" + mark.city 
+			];
+			var infowindow = new google.maps.InfoWindow({
+				content: cnt.join()
+			});
+			infowindow.open(marker.get('map'), marker);
+		});
+		
+		
+
+  
+	};
+	
 	$scope.mapCreated = function (map) {
 		console.log('map created');
 		$scope.map = map;
@@ -29,8 +49,8 @@ angular.module('starter.controllers', [])
 			.success(function(data, status, headers, config) {
 				console.log('got data', data);
 				for (var i=0; i<data.length; i++) {
-					var site = new google.maps.LatLng(data[i].lat, data[i].lng);
-					var marker = new google.maps.Marker({position: site, map: map,  title: data[i].name});
+					configureMarker(data[i], map);
+					
 				};
 				$scope.map.setCenter(new google.maps.LatLng(data[0].lat, data[0].lng));
 			})
